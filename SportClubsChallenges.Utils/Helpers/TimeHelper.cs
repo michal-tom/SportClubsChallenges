2dac44b349c922c0d1445b8f1ebb6af93127bce4
Michał Tomaszewski <michaltom@op.pl>
2021-03-27 21:01:40 +0100

    Add progress for challenge cards

--- SportClubsChallenges.Utils/Helpers/TimeHelper.cs
+++ SportClubsChallenges.Utils/Helpers/TimeHelper.cs
@@ -8,5 +8,59 @@
         {
             return DateTime.Today.AddDays(-(DateTime.Today.DayOfWeek - startOfWeek.Value));
         }
+
+        public static int GetPercentageProgress(DateTime startDate, DateTime endDate)
+        {
+            if (startDate.Date >= DateTime.Today)
+            {
+                return 0;
+            }
+
+            if (endDate.Date < DateTime.Today)
+            {
+                return 100;
+            }
+
+            var totalDuration = endDate.Date.Subtract(startDate.Date);
+            if (totalDuration.Days == 0)
+            {
+                return 0;
+            }
+
+            var currentDuration = DateTime.Today.Subtract(startDate.Date);
+
+            return (int) ((100 * currentDuration) / totalDuration);
+        }
+
+        public static string GetChallengeDatePeriodDescription(DateTime startDate, DateTime endDate)
+        {
+            if (startDate.Date > DateTime.Today)
+            {
+                var daysToStart = startDate.Date.Subtract(DateTime.Today).TotalDays;
+                if (daysToStart == 1)
+                {
+                    return $"Challenge will start tomorrow";
+                }
+                else
+                {
+                    return $"Challenge will start in {daysToStart} days";
+                }
+            }
+
+            if (endDate.Date < DateTime.Today)
+            {
+                return $"Challenge has already ended";
+            }
+
+            var daysToEnd = endDate.Date.Subtract(DateTime.Today).TotalDays;
+            if (daysToEnd == 0)
+            {
+                return $"Challenge ends today";
+            }
+            else
+            {
+                return $"Challenge will end in {daysToEnd} days";
+            }
+        }
     }
 }
