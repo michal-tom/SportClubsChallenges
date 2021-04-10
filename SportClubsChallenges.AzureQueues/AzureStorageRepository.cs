b0f16c7311186971f1eafd08520f7086ce25a991
Michał Tomaszewski <michaltom@op.pl>
2021-04-10 20:45:04 +0200

    Ping azure functions host when adding new message to queue

--- SportClubsChallenges.AzureQueues/AzureStorageRepository.cs
+++ SportClubsChallenges.AzureQueues/AzureStorageRepository.cs
@@ -1,17 +1,19 @@
-﻿
-namespace SportClubsChallenges.AzureQueues
+﻿namespace SportClubsChallenges.AzureQueues
 {
+    using System.Net;
+    using System.Threading.Tasks;
     using Azure.Storage.Queues;
     using Microsoft.Extensions.Configuration;
-    using System.Threading.Tasks;
 
     public class AzureStorageRepository : IAzureStorageRepository
     {
         private readonly string connectionString;
+        private readonly string azureFunctionsUrl;
 
         public AzureStorageRepository(IConfiguration configuration)
         {
             this.connectionString = configuration["ConnectionStrings:SportClubsChallengeStorage"];
+            this.azureFunctionsUrl = configuration["SportClubsChallengeAzureFunctionsUrl"];
         }
 
         public async Task CreateMessage(string queueName, string message)
@@ -20,11 +22,28 @@ namespace SportClubsChallenges.AzureQueues
             {
                 return;
             }
-            
+
             QueueClient queue = new QueueClient(this.connectionString, queueName, new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 });
             await queue.CreateIfNotExistsAsync();
 
             await queue.SendMessageAsync(message);
+
+            // TODO: temporary solution to ping azure functions related with queue
+            await PingAzureFunctionHost();
+        }
+
+        private async Task PingAzureFunctionHost()
+        {
+            if (string.IsNullOrEmpty(this.azureFunctionsUrl))
+            {
+                return;
+            }
+
+            await Task.Run(() =>
+            {
+                var request = (HttpWebRequest) WebRequest.Create(this.azureFunctionsUrl);
+                request.GetResponseAsync();
+            });
         }
     }
 }
